89e60bf09b7a6ba0a1b03150b8146c2c5a22603c
k-857 <60834543+k-857@users.noreply.github.com>
2022-01-25 19:49:05 -0500

    Update Convert.js

--- Convert.js
+++ Convert.js
@@ -1,3 +1,9 @@
-let f = prompt("Enter Fahrenheit Temperature");
-	f = perseint(f);
-	let c = (f-32)* 5/9;
+var f = prompt("Enter Farenheit temperature"); 
+
+    f = parseInt(f); 
+
+    let c = ((f - 32) * 5/9); 
+
+    c = parseInt(c); 
+    
+    alert("Farenheit temperature is " + f + "\n\nCelcius temperature is " + c);
